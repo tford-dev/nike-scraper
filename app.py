99cfdd3417ab0f0109c3f1e6e2db373e7170c58d
tford-dev <terranceford513@gmail.com>
2023-09-22 22:03:36 -0400

    Stopping point. Now prints data

--- app.py
+++ app.py
@@ -2,9 +2,27 @@ from selenium import webdriver
 
 from pages.nike_page import NikePage;
 
-chrome = webdriver.Chrome();
-chrome.get('https://www.nike.com/')
-page = NikePage(chrome);
+USER_CHOICE = """
+    Welcome to the Nike-Scraper 1.0
+    Enter:
+    - 'b' to begin query
+    - 'q' to quit
+"""
+
+def menu():
+    while True:
+        user_input = input(USER_CHOICE);
+        if user_input.lower() == 'b':   
+            chrome = webdriver.Chrome()
+            chrome.get('https://www.nike.com/')
+            page = NikePage(chrome)
+            page.navigate()
+            chrome.quit();
+        elif user_input.lower() == 'q':
+            print("Process terminated");
+            break;
+            
 
 
-page.navigate()
+    
+menu();
--- pages/nike_page.py
+++ pages/nike_page.py
@@ -11,13 +11,15 @@ class NikePage:
     @property
     def result_items(self):
         result_items_list = self.browser.find_elements(By.CSS_SELECTOR, "div.product-card__info")
-        for item in result_items_list:
-            print(item);
+        for index, item in enumerate(result_items_list):
+            print(f"-Result #{index}----------------");
+            print(item.text);
+        time.sleep(2)
         
     def search_bar(self):
         pre_search_button = self.browser.find_element(By.CSS_SELECTOR, "button.pre-search-btn")
         pre_search_button.click()
-        time.sleep(2)
+        time.sleep(1.5)
         # if pre_search_button:
         #     print("200")
         # else:
@@ -26,12 +28,10 @@ class NikePage:
         search_query = input("Type in some input for testing...: ")
         search_bar_on_web_page.send_keys(search_query);
         search_bar_on_web_page.send_keys(Keys.RETURN)
-      
-    def results_elements(self):
-        results_list = self.browser.find_elements(By.CSS_SELECTOR, "div.product-card");
-        print(len(results_list));
             
     def navigate(self):
-        self.search_bar();
-        self.results_elements();
-        self.result_items();
+        try:
+            self.search_bar();
+            self.result_items();
+        except TypeError:
+            pass
